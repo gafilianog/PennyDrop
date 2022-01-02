1dce38f4fa7446623e6efe33129fd1130d3a27b1
Gafiliano Gapili <gafiliano2@gmail.com>
2022-01-02 20:19:12 +0700

    Need to check player_list_item.xml android:selectedItemPosition attribute unknown

--- app/src/main/java/dev/gafilianog/pennydrop/types/NewPlayer.kt
+++ app/src/main/java/dev/gafilianog/pennydrop/types/NewPlayer.kt
@@ -1,11 +1,19 @@
 package dev.gafilianog.pennydrop.types
 
 import androidx.databinding.ObservableBoolean
+import dev.gafilianog.pennydrop.game.AI
 
 data class NewPlayer(
-    val playerName: String = "",
+    var playerName: String = "",
+    var isIncluded: ObservableBoolean = ObservableBoolean(false),
     val isHuman: ObservableBoolean = ObservableBoolean(true),
     val canBeRemoved: Boolean = true,
     val canBeToggled: Boolean = true,
-    var isIncluded: ObservableBoolean = ObservableBoolean(!canBeRemoved)
-)
+    var selectedAIPosition: Int = -1
+) {
+    fun selectedAI() = if (!isHuman.get()) {
+        AI.basicAI.getOrNull(selectedAIPosition)
+    } else {
+        null
+    }
+}
