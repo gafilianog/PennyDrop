c491965bd2dd3a2d54c1946b4253d3d3c53c2827
Gafiliano Gapili <gafiliano2@gmail.com>
2022-01-17 22:15:39 +0700

    SwitchCompat issue solved. Before removing themes

--- app/src/main/java/dev/gafilianog/pennydrop/types/NewPlayer.kt
+++ app/src/main/java/dev/gafilianog/pennydrop/types/NewPlayer.kt
@@ -5,10 +5,10 @@ import dev.gafilianog.pennydrop.game.AI
 
 data class NewPlayer(
     var playerName: String = "",
-    var isIncluded: ObservableBoolean = ObservableBoolean(false),
     val isHuman: ObservableBoolean = ObservableBoolean(true),
     val canBeRemoved: Boolean = true,
     val canBeToggled: Boolean = true,
+    var isIncluded: ObservableBoolean = ObservableBoolean(!canBeRemoved),
     var selectedAIPosition: Int = -1
 ) {
     fun selectedAI() = if (!isHuman.get()) {
