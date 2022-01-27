ce475525e49efe9716fa1e01966a502f32541dc4
Gafiliano Gapili <gafiliano2@gmail.com>
2022-01-27 10:34:27 +0700

    Chapter 4 done

--- app/src/main/java/dev/gafilianog/pennydrop/game/AI.kt
+++ app/src/main/java/dev/gafilianog/pennydrop/game/AI.kt
@@ -1,20 +1,37 @@
 package dev.gafilianog.pennydrop.game
 
-data class AI(val name: String) {
+import dev.gafilianog.pennydrop.types.Player
+import dev.gafilianog.pennydrop.types.Slot
+
+data class AI(
+    val aiId: Int = 0,
+    val name: String,
+    val rollAgain: (slots: List<Slot>)->Boolean
+) {
 
     override fun toString() = name
+
+    fun toPlayer() = Player(
+        playerName = name,
+        isHuman = false,
+        selectedAI = this
+    )
 
     companion object {
         @JvmStatic
         val basicAI = listOf(
-            AI("TwoFace"),
-            AI("No Go Noah"),
-            AI("Bail Out Beulah"),
-            AI("Fearful Fred"),
-            AI("Even Steven"),
-            AI("Riverboat Ron"),
-            AI("Sammy Sixes"),
-            AI("Random Rachel")
+            AI(1,"TwoFace") {slots ->
+                slots.fullSlots() < 3 || (slots.fullSlots() == 3 && coinFlipIsHeads())
+            },
+            AI(2,"No Go Noah") {slots -> slots.fullSlots() == 0 },
+            AI(3,"Bail Out Beulah") {slots -> slots.fullSlots() <= 1 },
+            AI(4,"Fearful Fred") {slots -> slots.fullSlots() <= 2 },
+            AI(5,"Even Steven") {slots -> slots.fullSlots() <= 3 },
+            AI(6,"Riverboat Ron") {slots -> slots.fullSlots() <= 4 },
+            AI(7,"Sammy Sixes") {slots -> slots.fullSlots() <= 5 },
+            AI(8,"Random Rachel") {coinFlipIsHeads()}
         )
     }
 }
+
+fun coinFlipIsHeads() = (Math.random() * 2).toInt() == 0
--- app/src/main/java/dev/gafilianog/pennydrop/viewmodels/GameViewModel.kt
+++ app/src/main/java/dev/gafilianog/pennydrop/viewmodels/GameViewModel.kt
@@ -2,8 +2,15 @@ package dev.gafilianog.pennydrop.viewmodels
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dev.gafilianog.pennydrop.game.GameHandler
+import dev.gafilianog.pennydrop.game.TurnEnd
+import dev.gafilianog.pennydrop.game.TurnResult
 import dev.gafilianog.pennydrop.types.Player
 import dev.gafilianog.pennydrop.types.Slot
+import dev.gafilianog.pennydrop.types.clear
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class GameViewModel: ViewModel() {
     private var players: List<Player> = emptyList()
@@ -21,20 +28,176 @@ class GameViewModel: ViewModel() {
     val currentTurnText = MutableLiveData("")
     val currentStandingsText = MutableLiveData("")
 
+    private var clearText = false
+
     fun startGame(playersForNewGame: List<Player>) {
         this.players = playersForNewGame
         this.currentPlayer.value =
             this.players.firstOrNull().apply {
                 this?.isRolling = true
             }
-        this.canRoll.value = true
+
+        canRoll.value = true
+        canPass.value = false
+
+        slots.value?.clear()
+        slots.notifyChange()
+
+        currentTurnText.value = "The game has begun!\n"
+        currentStandingsText.value = generateCurrentStandings(this.players)
     }
 
     fun roll() {
-        // TODO: Later
+        slots.value?.let { currentSlots ->
+            val currentPlayer = players.firstOrNull{it.isRolling}
+            if (currentPlayer != null && canRoll.value == true) {
+                updateFromGameHandler(
+                    GameHandler.roll(players, currentPlayer, currentSlots)
+                )
+            }
+        }
     }
 
     fun pass() {
-        // TODO: Later
+        val currentPlayer = players.firstOrNull{it.isRolling}
+        if (currentPlayer != null && canPass.value == true) {
+            updateFromGameHandler(
+                GameHandler.pass(players, currentPlayer)
+            )
+        }
     }
+
+    private fun <T> MutableLiveData<List<T>>.notifyChange() {
+        this.value = this.value
+    }
+
+    private fun generateCurrentStandings(
+        players: List<Player>,
+        headerText: String = "Current Standings: "
+    ) =
+        players.sortedBy { it.pennies }.joinToString(
+            separator = "\n",
+            prefix = "$headerText\n"
+        ) {
+            "\t${it.playerName} - ${it.pennies} pennies"
+        }
+
+    private fun updateFromGameHandler(result: TurnResult) {
+        if (result.currentPlayer != null) {
+            currentPlayer.value?.addPennies(result.coinChangeCount?:0)
+            currentPlayer.value = result.currentPlayer
+            this.players.forEach { player ->
+                player.isRolling = result.currentPlayer == player
+            }
+        }
+
+        if (result.lastRoll != null) {
+            slots.value?.let { currentSlots ->
+                updateSlots(result, currentSlots, result.lastRoll)
+            }
+        }
+
+        currentTurnText.value = generateTurnText(result)
+        currentStandingsText.value = generateCurrentStandings(this.players)
+
+        canRoll.value = result.canRoll
+        canPass.value = result.canPass
+
+        if (!result.isGameOver && result.currentPlayer?.isHuman == false) {
+            canRoll.value = false
+            canPass.value = false
+            playAITurn()
+        }
+    }
+
+    private fun updateSlots(
+        result: TurnResult,
+        currentSlots: List<Slot>,
+        lastRoll: Int
+    ) {
+        if (result.clearSlots) {
+            currentSlots.clear()
+        }
+
+        currentSlots.getOrNull(lastRoll - 1)?.also { slot ->
+            if (!result.clearSlots && slot.canBeFilled)
+                slot.isFilled = true
+
+            slot.lastRolled = true
+        }
+        slots.notifyChange()
+    }
+
+    private fun generateTurnText(result: TurnResult): String {
+        if (clearText)
+            currentTurnText.value = ""
+        clearText = result.turnEnd != null
+
+        val currentText = currentTurnText.value?: ""
+        val currentPlayerName = result.currentPlayer?.playerName?: "???"
+
+        return when {
+            result.isGameOver->
+                """
+                    |Game Over!
+                    |$currentPlayerName is the winner!
+                    |
+                    |${generateCurrentStandings(this.players, "Final Scores:\n")}
+                """.trimMargin()
+
+            result.turnEnd == TurnEnd.Bust->
+                "${ohNoPhrases.shuffled().first()} ${result.previousPlayer?.playerName} rolled a ${result.lastRoll}.  They collected ${result.coinChangeCount} pennies for a total of ${result.previousPlayer?.pennies}.\n$currentText"
+
+            result.turnEnd == TurnEnd.Pass->
+                "${result.previousPlayer?.playerName} passed.  They currently have ${result.previousPlayer?.pennies} pennies.\n$currentText"
+
+            result.lastRoll != null->
+                "$currentText\n$currentPlayerName rolled a ${result.lastRoll}."
+
+            else->""
+        }
+    }
+
+    private fun playAITurn() {
+        viewModelScope.launch {
+            delay(1000)
+            slots.value?.let { currentSlots->
+                val currentPlayer = players.firstOrNull{ it.isRolling }
+
+                if (currentPlayer != null && !currentPlayer.isHuman) {
+                    GameHandler.playAITurn(
+                        players,
+                        currentPlayer,
+                        currentSlots,
+                        canPass.value == true
+                    )?.let{ result->
+                        updateFromGameHandler(result)
+                    }
+                }
+            }
+        }
+    }
+
+    private val ohNoPhrases = listOf(
+        "Oh no!",
+        "Bummer!",
+        "Dang.",
+        "Whoops.",
+        "Ah, fiddlesticks.",
+        "Oh, kitty cats.",
+        "Piffle.",
+        "Well, crud.",
+        "Ah, cinnamon bits.",
+        "Ooh, bad luck.",
+        "Shucks!",
+        "Woopsie daisy.",
+        "Nooooooo!",
+        "Aw, rats and bats.",
+        "Blood and thunder!",
+        "Gee whillikins.",
+        "Well that's disappointing.",
+        "I find your lack of luck disturbing.",
+        "That stunk, huh?",
+        "Uff da."
+    )
 }
